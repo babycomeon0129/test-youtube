8f8c14956a33b1aee5e057717b42d05388f70198
Lynnn <lynn_huang@cmh.com.tw>
2024-08-14 14:08:01 +0800

    fix: fix error log

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,21 +2,19 @@
     <div class="home">
         <div class="container">
             <div class="video">
-                <div class="main__video">
+                <div v-if="mainVideo?.contentDetails?.videoId" class="main__video">
                     <div class="main__video__wrapper">
                         <iframe
                             :src="`https://www.youtube.com/embed/${mainVideo.contentDetails.videoId}?autoplay=1&mute=1&enablejsapi=1&loop=1&playlist=${mainVideo.contentDetails.videoId}`"
-                            title="YouTube video player"
-                            frameborder="0"
+                            title="YouTube video player" frameborder="0"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                            referrerpolicy="strict-origin-when-cross-origin"
-                            allowfullscreen
-                        />
+                            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen />
                     </div>
                     <h2>{{ mainVideo.snippet.title }}</h2>
                     <div class="main__video__info">
                         <div class="main__video__detail">
-                            <img src="https://yt3.ggpht.com/ytc/AIdro_lyTTRW4D3tELjeHN6WHC9F3En7wqhcN4-6rpPivkR2SZg=s48-c-k-c0x00ffffff-no-rj" />
+                            <img
+                                src="https://yt3.ggpht.com/ytc/AIdro_lyTTRW4D3tELjeHN6WHC9F3En7wqhcN4-6rpPivkR2SZg=s48-c-k-c0x00ffffff-no-rj" />
                             <div>{{ mainVideo.snippet.channelTitle }}</div>
                         </div>
                         <div class="main__video__description">
@@ -25,12 +23,8 @@
                     </div>
                 </div>
                 <div class="video__list">
-                    <div
-                        v-for="(video, index) in videoList"
-                        :key="video.id"
-                        class="video__list__wrapper"
-                        @click="clickTag(video)"
-                    >
+                    <div v-for="(video, index) in videoList" :key="video.id" class="video__list__wrapper"
+                        @click="clickTag(video)">
                         <template v-if="index !== 0">
                             <div class="video__image">
                                 <img :src="video.snippet.thumbnails.default.url" />
@@ -64,6 +58,7 @@ import axios from 'axios';
 
 let videoList = ref(null);
 let mainVideo = ref(null);
+let messageList = ref([]);
 
 
 const getData = async () => {
@@ -79,10 +74,33 @@ const getData = async () => {
     videoList.value = res.data.items;
     mainVideo.value = videoList.value.shift();
     console.log(res.data.items[0]);
+    getMessageList(res.data.items[0].id);
 }
 
-const clickTag = video => mainVideo.value = video;
+const getMessageList = async (id) => {
+    try {
+        let res = await axios.get('https://youtube.googleapis.com/youtube/v3/comments', {
+            params: {
+                part: 'id,snippet',
+                parentId: id,
+                key: 'AIzaSyB2C72BoGuN4_KmG0-cLf5yiOgsMFLu4XU'
+            }
+        })
+
+        console.log(res);
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
+
+const clickTag = video => {
+    mainVideo.value = video;
+    getMessageList(mainVideo.value.id);
+};
+
 getData();
+
 
 const testList = [
     {
@@ -128,4 +146,6 @@ const testList = [
 
 </script>
 
-<style lang="scss" scoped>@import '@/styles/home';</style>
+<style lang="scss" scoped>
+@import '@/styles/home';
+</style>
